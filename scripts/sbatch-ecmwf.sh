69fecd093f892b19b7d4a46389ea3030fd8799dd
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-07-18 14:35:47 +0000

    Examples for ECMWF

--- scripts/sbatch-ecmwf.sh
+++ scripts/sbatch-ecmwf.sh
@@ -1,2 +1,9 @@
-sbatch --mem=247000 --ntasks-per-node 256  -N1     -p par mpitest.sh 
-sbatch --mem=247000 --ntasks-per-node 256  -N1 -G4 -p gpu mpitest.sh 
+#!/bin/bash
+
+. ./gpupack.sh
+
+# Queue for parallel jobs
+sbatch                                     -N2     -p par mpitest.sh $GPUPACK_PREFIX/pack/49t0_compile_with_pgi_2303-field_api.01.NVHPC2305.1s t0107
+
+# Queue for GPU jobs; I have not managed to more than one node
+sbatch --mem=247000 --ntasks-per-node 256  -N1 -G4 -p gpu mpitest.sh $GPUPACK_PREFIX/pack/49t0_compile_with_pgi_2303-field_api.01.NVHPC2305.1s t0107
