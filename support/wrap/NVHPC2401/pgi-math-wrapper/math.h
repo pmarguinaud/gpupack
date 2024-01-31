3284a4d9151c6f34463f58927278952e8ab016d6
Judicaël Grasset <judicael.grasset@meteo.fr>
2024-01-31 14:19:03 +0000

    add wrapper for pgi 24.01

---
+++ support/wrap/NVHPC2401/pgi-math-wrapper/math.h
@@ -0,0 +1,16 @@
+/* 
+ *     Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * NVIDIA CORPORATION and its licensors retain all intellectual property
+ * and proprietary rights in and to this software, related documentation
+ * and any modifications thereto.  Any use, reproduction, disclosure or
+ * distribution of this software and related documentation without an express
+ * license agreement from NVIDIA CORPORATION is strictly prohibited.
+ *
+ */
+
+#undef __GNUC__
+#define __GNUC__ 6
+#include_next <math.h>
+#undef __GNUC__
+#define __GNUC__ 4
