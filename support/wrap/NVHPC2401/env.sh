e6c622f08f465a592a0480ef0935e2f3e4db0c57
Judicaël Grasset <judicael.grasset@meteo.fr>
2024-02-16 10:21:09 +0000

    remove need for LD_LIBRARY_PATH

--- support/wrap/NVHPC2401/env.sh
+++ support/wrap/NVHPC2401/env.sh
@@ -3,16 +3,13 @@ NVHPC_VERSION=24.1
 CUDA_VERSION=12.3
 
 PREFIX=""
-GCC_LIBARAY_PATH=""
 
 case $(hostname) in
 	*belenos*)
 		PREFIX=/opt/softs
-		GCC_LIBARAY_PATH=/opt/softs/gcc/9.2.0/lib64
 		;;
 	*taranis*)
 		PREFIX=/opt/softs
-		GCC_LIBARAY_PATH=/opt/softs/gcc/9.2.0/lib64
 		;;
 	*bullx*)
 		PREFIX=/ec/res4/hpcperm/sor/install
@@ -29,6 +26,3 @@ NVHPC_CUDA_HOME="$NVHPC_PREFIX/$CUDA_PREFIX"
 
 echo "export NVHPC_CUDA_HOME=$NVHPC_CUDA_HOME"
 echo "export CPATH=$NVHPC_PREFIX/comm_libs/$CUDA_VERSION/hpcx/latest/ompi/include\${CPATH:+:\$CPATH}"
-if [ ! -z $GCC_LIBARAY_PATH ]; then
-	echo "export LD_LIBRARY_PATH=$GCC_LIBARAY_PATH\${LD_LIBRARY_PATH:+:\$LD_LIBRARY_PATH}"
-fi
